642c9e3ae61813d05f122acbaecedbc32fef868b
pqpedro <pedrolbarriola@gmail.com>
2022-11-03 19:08:03 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,26 +1,59 @@
 const shop = document.getElementById("shop");
 
+const shopItemsData = [
+  {
+    id: "cS1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "oS1",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "tS1",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "mS1",
+    name: "Men Suits ",
+    price: 300,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
+
 const generateShop = () => {
-  return (shop.innerHTML = `
-    <div class="item"> 
-        <img width="300" src="images/img-1.jpg" alt="" />
+  return (shop.innerHTML = shopItemsData.map((x) => {
+    const {id, name, price, desc, img} =x
+    return `
+    <div id=product-id-${id} class="item"> 
+        <img width="300" src=${img} alt="" />
         <div class="details">
-          <h3>Casual Shirt</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-          <br>
+          <h3>${name} </h3>
+          <p>${desc}</p>
            <div class="price-quantity">
-              <h2>$0</h2>
+              <h2>$ ${price} </h2>
               <div class="buttons">
                 <!-- <i class="bi bi-dash-lg decrease"></i> -->
                 <button class="decrease">-</button>
-                <div class="quantity">0</div>
+                <div id=${id} class="quantity">0</div>
                 <button class="increase">+</button>
                 <!-- <i class="bi bi-plus-lg increase"></i> -->
             </div>
           </div>
         </div>
       </div>
-    `);
+    `;
+  }).join(""));
 };
 
-generateShop()
+generateShop();
